a19d77c4dd64190da7bee5340cfb39572fcc4b85
mateustech <mateustech686@gmail.com>
2019-12-27 09:20:45 -0300

    Escrevendo Arquivo

--- spider.js
+++ spider.js
@@ -1,16 +1,18 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
-request('https://www.imdb.com/chart/moviemeter', function (err, res, body) {
+request('http://www.imdb.com/chart/moviemeter', function (err, res, body) {
   if (err) {
     console.log('Deu Ruim Cara :(' + err)
-    return
   }
   var $ = cheerio.load(body)
   $('.lister-list tr').each(function () {
     var title = $(this).find('.titleColumn a').text().trim()
     var Nota = $(this).find('.imdbRating strong').text().trim()
 
-    console.log('Titulo' + title + 'Nota' + Nota)
+    console.log('Titulo ' + title + ' Nota ' + Nota)
+
+    //fs.appendFile('imdb.txt', title + ' ' + Nota + '\n')
   })
 })
